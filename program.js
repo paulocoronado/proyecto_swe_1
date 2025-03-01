8dd4f81ecf37811c1b85e5f96b32577bfbbf2aa7
Paulo Coronado <paulocoronado@gmail.com>
2025-03-01 11:45:32 -0500

    v 0.0.2 Mapa base

---
+++ program.js
@@ -0,0 +1,7 @@
+var map = L.map('map').setView([4.622283900850308, -74.17107456828663], 16);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+
